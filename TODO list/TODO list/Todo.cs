8e285b76ae54d8c935eebf48f96ad4fcc47ef2b3
Irina Tumashevich <s3tuir00@students.osao.fi>
2023-11-29 19:16:08 +0200

    Class Todo: get, set for fields

--- TODO list/TODO list/Todo.cs
+++ TODO list/TODO list/Todo.cs
@@ -1,20 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TODO_list
 {
+
+    //New class
     internal class Todo
     {
+        //Fields
         private string text;
         private bool check;
 
+        //Constructor
         public Todo()
         {
-            this.text = string.Empty;
+            this.text = "";
             this.check = false;
+        }
+
+        //get, set properties
+        public string TextNow
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    this.text = value;
+                }
+            }
+        }
+
+
+        public bool CheckNow
+        {
+            get
+            {
+                return this.check;
+            }
+            set
+            {
+                this.check = value;
+            }
         }
 
     }
