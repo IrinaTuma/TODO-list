9dbc04015de87ed86f9397911418506c2bc97479
Irina Tumashevich <s3tuir00@students.osao.fi>
2023-11-29 18:41:29 +0200

    New class Todo, 2 fields, constructor

---
+++ TODO list/TODO list/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_list
+{
+    internal class Todo
+    {
+        private string text;
+        private bool check;
+
+        public Todo()
+        {
+            this.text = string.Empty;
+            this.check = false;
+        }
+
+    }
+}
